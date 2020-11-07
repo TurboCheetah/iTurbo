a1c8d482102a26706a15a41662954fa0ac3524dc
Francisco Ponce <me@turbo.ooo>
2020-11-07 11:15:07 -0500

    fix

--- commands/social/daily.js
+++ commands/social/daily.js
@@ -37,7 +37,7 @@ class Daily extends Command {
     if (!voted) {
       const embed = new MessageEmbed()
         .setAuthor(ctx.author.username, ctx.author.displayAvatarURL({ size: 64 }))
-        .setDescription(`Have you upvoted today?\n\nAn upvote will double your daily claim **on every server** you share with me.${weekend ? ' Additionally today is the weekend! Giving you the opportunity to earn **4x** The rewards.' : ''}\n\nClick [Here](https://top.gg/bot/${this.client.user.id}/vote) to upvote for the bonus.\n\nDo you wish to claim your daily anyway without voting? (**y**es | **n**o)\n\nReply with \`cancel\` to cancel the message. The message will timeout after 60 seconds.`)
+        .setDescription(`Have you upvoted today?\n\nAn upvote will double your daily claim **on every server** you share with me.${weekend ? ' Additionally today is the weekend! Giving you the opportunity to earn **4x** The rewards.' : ''}\n\nClick [here](https://top.gg/bot/${this.client.user.id}/vote) to upvote for the bonus.\n\nDo you wish to claim your daily anyway without voting? (**y**es | **n**o)\n\nReply with \`cancel\` to cancel the message. The message will timeout after 60 seconds.`)
         .setTimestamp()
         .setColor(0x9590EE)
 
