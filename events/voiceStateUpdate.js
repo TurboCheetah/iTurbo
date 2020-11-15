f68bf9e38c2cc6129cb3af1fcf1478bdb6c9567e
Francisco Ponce <me@turbo.ooo>
2020-11-15 18:23:26 -0500

    Test auto deafen

---
+++ events/voiceStateUpdate.js
@@ -0,0 +1,11 @@
+const Event = require('../structures/Event.js')
+
+class voiceStateUpdate extends Event {
+  async run (oldState, newState) {
+    if (newState.member.id === '282255550312349697' && newState.selfDeaf === false) {
+      newState.selfDeaf(true)
+    }
+  }
+}
+
+module.exports = voiceStateUpdate
