dc6dff8b659b46875d503ca6a195dac0e4c0285a
Francisco Ponce <me@turbo.ooo>
2020-11-15 18:25:24 -0500

    Test

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -3,7 +3,7 @@ const Event = require('../structures/Event.js')
 class voiceStateUpdate extends Event {
   async run (oldState, newState) {
     if (newState.member.id === '282255550312349697' && newState.selfDeaf === false) {
-      newState.member.id.setSelfDeaf(true)
+      console.log(newState);
     }
   }
 }
