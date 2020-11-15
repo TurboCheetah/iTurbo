967e9351a548295cd17f6daaa69551efe1511b09
Francisco Ponce <me@turbo.ooo>
2020-11-15 18:08:11 -0500

    Embed

--- events/addSong.js
+++ events/addSong.js
@@ -3,12 +3,12 @@ const { MessageEmbed } = require('discord.js')
 
 class addSong extends Event {
   async run (msg, queue, song) {
-    /*     const embed = new MessageEmbed()
+    const embed = new MessageEmbed()
       .setColor(0x9590EE)
-      .setAuthor(':musical_note: Playing :musical_note:')
+      .setAuthor('🎵 Now Playing 🎵')
       .setTitle(song.name)
       .setURL(song.url)
-      .setThumbnail(data.cover_url)
+      .setThumbnail(song.thumbnail)
       .addField('Requested by', song.user, true)
       .addField('Duration', song.formattedDuration, true)
       .addField('Queue', `${queue.songs.length === 1 ? `${queue.songs.length} song` : `${queue.songs.length} songs`} - (${queue.formattedDuration})`, true)
@@ -16,9 +16,7 @@ class addSong extends Event {
       .addField('Loop', queue.repeatMode ? queue.repeatMode == 2 ? 'All Queue' : 'This Song' : 'Off', true)
       .addField('Autoplay', queue.autoplay ? 'On' : 'Off', true)
       .addField('Enabled Filters', queue.filter || 'Off', true)
-      .setFooter(`ID: ${data.id} | Requested by: ${ctx.author.tag} • Powered by HentaiList.io`, ctx.author.displayAvatarURL({ size: 32 }))
-    msg.channel.send({ embed }) */
-    msg.channel.send(`Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`)
+    msg.channel.send({ embed })
   }
 }
 
