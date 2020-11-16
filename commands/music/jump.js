71f5430d394d7a1abdaa75db2ed5bd66589d6d38
Francisco Ponce <me@turbo.ooo>
2020-11-15 23:00:21 -0500

    Jump command

---
+++ commands/music/jump.js
@@ -0,0 +1,23 @@
+const Command = require('../../structures/Command.js')
+
+class Jump extends Command {
+  constructor (...args) {
+    super(...args, {
+      description: 'Jump to the song number in the queue',
+      aliases: [],
+      botPermissions: ['CONNECT', 'SPEAK'],
+      usage: 'jump <song number>',
+      guildOnly: true,
+      cost: 0,
+      cooldown: 20
+    })
+  }
+
+  async run (ctx, args) {
+    if (!args.length) return ctx.reply('What song do you want me to skip to? Please provide a valid song number!')
+
+    this.client.distube.jump(ctx.message, parseInt(args[0] + 2))
+  }
+}
+
+module.exports = Jump
