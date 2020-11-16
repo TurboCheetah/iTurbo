a307748e405d746586d1062e6c906a943efc4349
Francisco Ponce <me@turbo.ooo>
2020-11-16 01:44:37 -0500

    Fix

--- structures/MiyakoClient.js
+++ structures/MiyakoClient.js
@@ -31,7 +31,7 @@ class MiyakoClient extends Client {
     this.events = new EventStore(this)
     this.sweeper = new MemorySweeper(this)
     this.responses = require('../utils/responses.js')
-    this.img = new imgapi.Client()
+    this.img = new imgapi.Client({host: this.config.imgapi})
     this.distube = new DisTube(this, { searchSongs: true, emitNewSongOnly: true, highWaterMark: 1 << 25 }) // Distube instance for playing music
 
     // More DisTube stuff
