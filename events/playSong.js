28c9ca66d0293e28d7e603bc8cfe901b0d73d130
Francisco Ponce <me@turbo.ooo>
2020-11-15 19:17:27 -0500

    Fix

--- events/playSong.js
+++ events/playSong.js
@@ -11,7 +11,7 @@ class playSong extends Event {
       .setThumbnail(song.thumbnail)
       .addField('Requested by', song.user, true)
       .addField('Duration', song.formattedDuration, true)
-      .addField('Queue', `${queue.songs.length === 1 ? `1 song` : `${queue.songs.length} songs`} - (${queue.formattedDuration})`, true)
+      .addField('Queue', `${queue.songs.length === 1 ? `1 song` : `${queue.songs.length} songs`} - ${queue.formattedDuration}`, true)
       .addField('Volume', `${queue.volume}%`, true)
       .addField('Loop', queue.repeatMode ? queue.repeatMode == 2 ? 'All Queue' : 'This Song' : 'Off', true)
       .addField('Autoplay', queue.autoplay ? 'On' : 'Off', true)
