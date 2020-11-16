ee2dd39316d9705425e2b60ee71facbec4dabdad
Francisco Ponce <me@turbo.ooo>
2020-11-15 22:56:55 -0500

    Resume command

---
+++ commands/music/resume.js
@@ -0,0 +1,26 @@
+const Command = require('../../structures/Command.js')
+
+class Resume extends Command {
+  constructor (...args) {
+    super(...args, {
+      description: 'Resumes the queue',
+      aliases: [],
+      botPermissions: ['CONNECT', 'SPEAK'],
+      usage: 'resume',
+      guildOnly: true,
+      cost: 0,
+      cooldown: 20
+    })
+  }
+
+  async run (ctx) {
+    if (!this.client.distube.isPaused(ctx.message)) {
+        return ctx.reply('The queue has not been paused!')
+    }
+
+    this.client.distube.resume(ctx.message)
+    ctx.reply('▶ Resumed')
+  }
+}
+
+module.exports = Resume
