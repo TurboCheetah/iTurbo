4aeebd544ba7075309f85fd350a095a677c2b8d3
Francisco Ponce <me@turbo.ooo>
2020-11-15 19:16:18 -0500

    Finish playlist embed

--- events/playList.js
+++ events/playList.js
@@ -3,23 +3,20 @@ const { MessageEmbed } = require('discord.js')
 
 class playList extends Event {
   async run (msg, queue, playlist, song) {
-/*         const embed = new MessageEmbed()
+    const embed = new MessageEmbed()
       .setColor(0x9590EE)
-      .setAuthor('🎵 Now Playing 🎵')
-      .setTitle(playlist.title)
-      .setURL(playlist.url)
+      .setAuthor(`🎵 Now Playing ${playlist.name} 🎵`)
+      .setTitle(`Now playing ${playlist.songs[0].name}`)
+      .setURL(playlist.songs[0].url)
       .setThumbnail(playlist.thumbnail)
       .addField('Requested by', song.user, true)
-      .addField('Duration', song.formattedDuration, true)
-      .addField('Queue', `${queue.songs.length === 1 ? `${queue.songs.length} song` : `${queue.songs.length} songs`} - (${queue.formattedDuration})`, true)
+      .addField('Length', `${playlist.songs.length === 1 ? `1 song` : `${playlist.songs.length} songs`} - ${playlist.formattedDuration}`, true)
+      .addField('Queue', `${queue.songs.length === 1 ? `1 song` : `${queue.songs.length} songs`} - (${queue.formattedDuration})`, true)
       .addField('Volume', `${queue.volume}%`, true)
       .addField('Loop', queue.repeatMode ? queue.repeatMode == 2 ? 'All Queue' : 'This Song' : 'Off', true)
       .addField('Autoplay', queue.autoplay ? 'On' : 'Off', true)
-      .addField('Enabled Filters', queue.filter || 'Off', true)
-      .setFooter(`ID: ${data.id} | Requested by: ${ctx.author.tag} • Powered by HentaiList.io`, ctx.author.displayAvatarURL({ size: 32 }))
+      .addField('Enabled Filters', queue.filter || 'No filters have been enabled', true)
     msg.channel.send({ embed })
-    msg.channel.send(`Play \`${playlist.title}\` playlist (${playlist.total_items} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`) */
-    console.log(`Name: ${playlist.name}\nTitle: ${playlist.title}\nTotal: ${playlist.total_items}\nItems: ${playlist.formattedDuration}`);
   }
 }
 
