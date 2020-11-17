71ff3ada4d4e85be7f28251582474107b2bcbf08
Francisco Ponce <me@turbo.ooo>
2020-11-17 18:03:05 -0500

    Update premium roleID

--- utils/constants.js
+++ utils/constants.js
@@ -2,7 +2,7 @@
 module.exports = {
   ownerID: '120306174225678336',
   mainGuildID: '221013455342141440',
-  premiumRole: '476467857543266316',
+  premiumRole: '615930972797534216',
   color: 0x9590EE,
   typing: '<a:typing:485594750871928833>',
   crossmark: '<a:animatedcrossmark:519166256214048769>',
