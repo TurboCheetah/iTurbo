bc86f0e0f944f8aa4d3a5034ec1ee662936edc95
Francisco Ponce <me@turbo.ooo>
2020-11-18 08:39:51 -0500

    Add awaitReply if args are not present

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -1,4 +1,5 @@
 const Command = require('../../structures/Command.js')
+const { MessageEmbed } = require('discord.js')
 
 class Loop extends Command {
   constructor (...args) {
@@ -14,16 +15,44 @@ class Loop extends Command {
   }
 
   async run (ctx, args) {
-    // if (!args.length) return ctx.reply('What do you want me to play? Please provide a search query or song url!')
+    if (!args.length) {
+      const embed = new MessageEmbed()
+        .setAuthor(ctx.author.username, ctx.author.displayAvatarURL({ size: 64 }))
+        .setDescription('Would you like to loop the current **song**, **queue**, or **disable** looping?\n\nReply with `cancel` to cancel the message. The message will timeout after 60 seconds.')
+        .setTimestamp()
+        .setColor(0x9590EE)
+
+      const filter = (msg) => msg.author.id === ctx.author.id
+      const response = await ctx.message.awaitReply('', filter, 60000, embed)
+      if (!response) return ctx.reply('No reply within 60 seconds. Time out.')
+
+      if (['on', 'enable', 'song'].includes(response.toLowerCase())) {
+        this.client.distube.setRepeatMode(ctx.message, 1)
+        return ctx.reply('Enabled looping for the current song.')
+      } else if (['queue'].includes(response.toLowerCase())) {
+        this.client.distube.setRepeatMode(ctx.message, 2)
+        return ctx.reply('Enabled looping for the queue.')
+      } else if (['off', 'disable'].includes(response)) {
+        this.client.distube.setRepeatMode(ctx.message, 0)
+        return ctx.reply('Disabled looping.')
+      } else if (['cancel'].includes(response)) {
+        return ctx.reply('Operation cancelled.')
+      } else {
+        return ctx.reply('Invalid response, please try again.')
+      }
+    }
     switch (args[0]) {
       case 'disable' || 'off':
         this.client.distube.setRepeatMode(ctx.message, 0)
+        ctx.reply('Disabled looping.')
         break
       case 'song':
         this.client.distube.setRepeatMode(ctx.message, 1)
+        ctx.reply('Enabled looping for the current song.')
         break
       case 'queue':
         this.client.distube.setRepeatMode(ctx.message, 2)
+        ctx.reply('Enabled looping for the queue.')
         break
       default:
         ctx.reply('Invalid option!')
