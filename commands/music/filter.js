640d92b5ce8aa472fbf1612d15d9b5e372abc903
Francisco Ponce <me@turbo.ooo>
2020-11-21 15:18:27 -0500

    Fix

--- commands/music/filter.js
+++ commands/music/filter.js
@@ -15,113 +15,72 @@ class Filter extends Command {
   }
 
   async run(ctx, args) {
+    const embed = new MessageEmbed()
+    .setColor(0x9590EE)
+    .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
+
     switch (args[0]) {
       case 'list':
         ctx.reply('Available filters:\n```\n3D\nbassboost\necho\nkaraoke\nnightcore\nvaporwave\nflanger\ngate\nhaas\nreverse\nsurround\nmcompand\nphaser\ntremolo\nearwax```')
         break
       case '3d':
         this.client.distube.setFilter(ctx.message, '3d')
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       case 'bassboost':
         this.client.distube.setFilter(ctx.message, args[0].toLowerCase())
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       case 'echo':
         this.client.distube.setFilter(ctx.message, args[0].toLowerCase())
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       case 'karaoke':
         this.client.distube.setFilter(ctx.message, args[0].toLowerCase())
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       case 'nightcore':
         this.client.distube.setFilter(ctx.message, args[0].toLowerCase())
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       case 'vaporwave':
         this.client.distube.setFilter(ctx.message, args[0].toLowerCase())
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       case 'flanger':
         this.client.distube.setFilter(ctx.message, args[0].toLowerCase())
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       case 'gate':
         this.client.distube.setFilter(ctx.message, args[0].toLowerCase())
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       case 'haas':
         this.client.distube.setFilter(ctx.message, args[0].toLowerCase())
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       case 'reverse':
         this.client.distube.setFilter(ctx.message, args[0].toLowerCase())
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       case 'surround':
         this.client.distube.setFilter(ctx.message, args[0].toLowerCase())
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       case 'mcompand':
         this.client.distube.setFilter(ctx.message, args[0].toLowerCase())
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       case 'phaser':
         this.client.distube.setFilter(ctx.message, args[0].toLowerCase())
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       case 'tremolo':
         this.client.distube.setFilter(ctx.message, args[0].toLowerCase())
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       case 'earwax':
         this.client.distube.setFilter(ctx.message, args[0].toLowerCase())
-        const embed = new MessageEmbed()
-          .setColor(0x9590EE)
-          .setAuthor(`| Enabled \`${args[0].toLowerCase()}\` filter`, ctx.author.displayAvatarURL({ size: 512 }))
         ctx.reply({ embed })
         break
       default:
