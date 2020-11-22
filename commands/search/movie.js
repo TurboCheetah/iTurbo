c55f352ae3094469db66edb024b95754d9d4ffce
Francisco Ponce <me@turbo.ooo>
2020-11-21 19:19:45 -0500

    Add Color and footer

--- commands/search/movie.js
+++ commands/search/movie.js
@@ -31,10 +31,11 @@ class Movie extends Command {
     if (!movie) throw `I couldn't find a movie with title **${query}** in page ${page}.`
 
     const embed = new MessageEmbed()
+    .setColor(0x9590EE)
       .setImage(`https://image.tmdb.org/t/p/original${movie.poster_path}`)
       .setTitle(`${movie.title} (${page} out of ${body.results.length} results)`)
       .setDescription(movie.overview)
-      .setFooter(`${this.client.user.username} uses the TMDb API but is not endorsed or certified by TMDb.`,
+      .setFooter(`Powered by TheMovieDB`,
         'https://www.themoviedb.org/assets/1/v4/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png')
     if (movie.title !== movie.original_title) embed.addField('Original Title', movie.original_title, true)
     embed
