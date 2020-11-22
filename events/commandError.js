7bff78f91bfdeaeb6e5ec05f823a25261d77feb6
Francisco Ponce <me@turbo.ooo>
2020-11-22 18:15:09 -0500

    Error emoji in commandError event

--- events/commandError.js
+++ events/commandError.js
@@ -6,7 +6,7 @@ class CommandError extends Event {
     // Allow `throw "Error message"` to rewind stack and reply from nested calls from a command.
     if (typeof err === 'string') return ctx.reply(err)
     console.log(`[COMMAND] ${ctx.command.name}: ${err.stack || err}`)
-    await ctx.reply(`Uh! Something went wrong unexpectedly!${ctx.author.id !== this.client.constants.ownerID ? " Don't worry my master will keep track of the problem and fix it soon." : ''}`)
+    await ctx.reply(`${this.client.constants.error} Uh! Something went wrong unexpectedly!${ctx.author.id !== this.client.constants.ownerID ? " Don't worry my master will keep track of the problem and fix it soon." : ''}`)
     const channel = this.client.channels.cache.get('735638949770559569')
     if (!channel) return
     const embed = new MessageEmbed()
