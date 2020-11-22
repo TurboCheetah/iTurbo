a1ca940969450c922b5493b499f0d02de2ee07d7
Francisco Ponce <me@turbo.ooo>
2020-11-21 19:45:09 -0500

    Success and Error emojis

--- utils/constants.js
+++ utils/constants.js
@@ -5,6 +5,6 @@ module.exports = {
   premiumRole: '615930972797534216',
   color: 0x9590EE,
   typing: '<a:typing:485594750871928833>',
-  crossmark: '<a:animatedcrossmark:519166256214048769>',
-  checkmark: '<a:animatedcheckmark:519166152488910850>'
+  error: '<:error:779869884296986644>',
+  success: '<:success:779869928735375380>'
 }
