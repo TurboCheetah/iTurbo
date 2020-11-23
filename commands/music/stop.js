ac52ab3bf8836b39e6352e9a6f42f4e009d6dfcd
Francisco Ponce <me@turbo.ooo>
2020-11-22 19:27:50 -0500

    Allow stopping when there isn't a queue

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -5,7 +5,7 @@ class Stop extends Command {
   constructor (...args) {
     super(...args, {
       description: 'Stops the queue',
-      aliases: [],
+      aliases: ['leave'],
       botPermissions: ['CONNECT', 'SPEAK', 'EMBED_LINKS'],
       usage: 'stop',
       guildOnly: true,
@@ -17,13 +17,18 @@ class Stop extends Command {
   async run (ctx) {
     const queue = this.client.distube.getQueue(ctx.message)
     this.client.distube.stop(ctx.message)
-    /*     if (queue || queue !== undefined) {
-      this.client.distube.stop(ctx.message)
+    if (!queue || queue !== undefined) {
+      const member = await this.verifyMember(ctx, ctx.author, true)
+      const channel = member.voice.channel
+
+      if (!channel) return ctx.reply(`${this.client.constants.error} You are not in a voice channel!`)
+
+      member.voice.channel.leave()
       const embed = new MessageEmbed()
         .setColor(0x9590EE)
         .setAuthor('| 🛑 Stopped', ctx.author.displayAvatarURL({ size: 512 }))
       return ctx.reply({ embed })
-    } */
+    }
 
     const embed = new MessageEmbed()
       .setColor(0x9590EE)
