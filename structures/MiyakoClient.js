881f4e5c3c6b4cae14d5ef55dc14b084898acc16
Francisco Ponce <me@turbo.ooo>
2020-11-23 23:59:15 -0500

    Update version

--- structures/MiyakoClient.js
+++ structures/MiyakoClient.js
@@ -33,7 +33,7 @@ class MiyakoClient extends Client {
     this.responses = require('../utils/responses.js')
     this.img = new imgapi.Client({ host: this.config.imgapi })
     this.distube = new DisTube(this, { searchSongs: true, emitNewSongOnly: true, highWaterMark: 1 << 25, customFilters: { purebass: 'bass=g=20,dynaudnorm=f=200,asubboost' } }) // Distube instance for playing music
-    this.version = '1.0.4'
+    this.version = '1.0.5'
 
     // More DisTube stuff
     this.distube.on('playSong', (msg, queue, song) => this.emit('playSong', msg, queue, song))
