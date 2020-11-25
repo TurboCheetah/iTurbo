552e5140bb7ba5250769f37f4dfff84a5ac5e72d
Francisco Ponce <me@turbo.ooo>
2020-11-25 10:16:16 -0500

    Add warning

--- commands/music/playlist.js
+++ commands/music/playlist.js
@@ -14,7 +14,8 @@ class Playlist extends Command {
   async run (ctx, [action = 'list', ...args]) {
     if (!['add', 'remove', 'list'].includes(action)) { return ctx.reply(`Usage: \`${ctx.guild.prefix}${this.usage}\``) }
 
-    return this[action](ctx, args)
+    //return this[action](ctx, args)
+    return ctx.reply('This command is under construction!')
   }
 
   async add (ctx, args) {
