cec4944025b3895d440729a9ae5e66c72d984f05
Francisco Ponce <me@turbo.ooo>
2020-11-26 18:37:16 -0500

    Standard

--- events/playList.js
+++ events/playList.js
@@ -13,7 +13,7 @@ class playList extends Event {
       .addField('Length', `${playlist.songs.length === 1 ? '1 song' : `${playlist.songs.length} songs`} - ${playlist.formattedDuration}`, true)
       .addField('Queue', `${queue.songs.length === 1 ? '1 song' : `${queue.songs.length} songs`} - ${queue.formattedDuration}`, true)
       .addField('Volume', `${queue.volume}%`, true)
-      .addField('Loop', queue.repeatMode ? queue.repeatMode == 2 ? 'All Queue' : 'This Song' : 'Off', true)
+      .addField('Loop', queue.repeatMode ? queue.repeatMode === 2 ? 'All Queue' : 'This Song' : 'Off', true)
       .addField('Autoplay', queue.autoplay ? 'On' : 'Off', true)
       .addField('Enabled Filters', queue.filter || 'No filters have been enabled', true)
     msg.channel.send({ embed })
