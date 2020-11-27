28a46e30f3bc8b469055e27efd5a9edf44013126
Francisco Ponce <me@turbo.ooo>
2020-11-27 11:45:40 -0500

    Test

--- commands/utility/crypto.js
+++ commands/utility/crypto.js
@@ -60,7 +60,7 @@ class Crypto extends Command {
       console.error(err)
     })
 
-    console.log(data[0].name);
+    console.log(data[0]);
     const embed = new MessageEmbed()
       .setColor(0x9590EE)
       .setAuthor(data[0].name, data[0].logo_url)
