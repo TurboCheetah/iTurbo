69548c1d10af7de2fabbb711fabf1a116a4c9f3f
Francisco Ponce <me@turbo.ooo>
2020-11-30 16:44:42 -0500

    Test

--- structures/PostgresGiveawaysManager.js
+++ structures/PostgresGiveawaysManager.js
@@ -4,13 +4,13 @@ const PostgresGiveawaysManager = class extends GiveawaysManager {
   // This function is called when the manager needs to get all the giveaway stored in the database.
   async getAllGiveaways () {
     // Get all the giveaway in the database
-    return await this.client.settings.bot.get('giveaways') || []
+    return await this.client.settings.bot.get('0').giveaways || []
   }
 
   // This function is called when a giveaway needs to be saved in the database (when a giveaway is created or when a giveaway is edited).
   async saveGiveaway (messageID, giveawayData) {
     // Get existing giveaways to append to.
-    const data = await this.client.settings.bot.get('giveaways') || []
+    const data = this.client.settings.bot.get('0').giveaways || []
 
     // Add the new one
     data.push(giveawayData)
@@ -23,7 +23,7 @@ const PostgresGiveawaysManager = class extends GiveawaysManager {
 
   async editGiveaway (messageID, giveawayData) {
     // Gets all the current giveaways
-    const giveaways = await this.client.settings.bot.get('giveaways') || []
+    const giveaways = this.client.settings.bot.get('0').giveaways || []
     // Remove the old giveaway from the current giveaways ID
     const newGiveawaysArray = giveaways.filter((giveaway) => giveaway.messageID !== messageID)
     // Push the new giveaway to the array
@@ -36,7 +36,7 @@ const PostgresGiveawaysManager = class extends GiveawaysManager {
 
   // This function is called when a giveaway needs to be deleted from the database.
   async deleteGiveaway (messageID) {
-    const db = await this.client.settings.bot.get('giveaways') || []
+    const db = this.client.settings.bot.get('0').giveaways || []
     // Remove the giveaway from the array
     const newGiveawaysArray = db.filter((giveaway) => giveaway.messageID !== messageID)
     // Save the updated array
