fdd0fa0ea6e8275c6148ee6dd6a483062952376d
Francisco Ponce <me@turbo.ooo>
2020-11-30 17:31:55 -0500

    Test

--- structures/PostgresGiveawaysManager.js
+++ structures/PostgresGiveawaysManager.js
@@ -4,19 +4,19 @@ const PostgresGiveawaysManager = class extends GiveawaysManager {
   // This function is called when the manager needs to get all the giveaway stored in the database.
   async getAllGiveaways () {
     // Get all the giveaway in the database
-    return await this.client.settings.bot.get('0').giveaways || []
+    return await this.client.settings.giveaways.find() || []
   }
 
   // This function is called when a giveaway needs to be saved in the database (when a giveaway is created or when a giveaway is edited).
   async saveGiveaway (messageID, giveawayData) {
     // Get existing giveaways to append to.
     console.log(giveawayData);
-    const data = this.client.settings.bot.get('0').giveaways || []
+    const data = this.client.settings.giveaways.get(messageID) || []
 
     // Add the new one
     data.push(giveawayData)
 
-    await this.client.settings.bot.update('0', { giveaways: data })
+    await this.client.settings.giveaways.update(messageID, { messageID: giveawayData.messageID, channelID: giveawayData.channelID, guildID: giveawayData.guildID })
 
     // Don't forget to return something!
     return true
@@ -24,24 +24,24 @@ const PostgresGiveawaysManager = class extends GiveawaysManager {
 
   async editGiveaway (messageID, giveawayData) {
     // Gets all the current giveaways
-    const giveaways = this.client.settings.bot.get('0').giveaways || []
+    const giveaways = this.client.settings.giveaways.get(messageID) || []
     // Remove the old giveaway from the current giveaways ID
     const newGiveawaysArray = giveaways.filter((giveaway) => giveaway.messageID !== messageID)
     // Push the new giveaway to the array
     newGiveawaysArray.push(giveawayData)
     // Save the updated array
-    await this.client.settings.bot.update('0', { giveaways: newGiveawaysArray })
+    await this.client.settings.giveaways.update(messageID, { newGiveawaysArray })
     // Don't forget to return something!
     return true
   }
 
   // This function is called when a giveaway needs to be deleted from the database.
   async deleteGiveaway (messageID) {
-    const db = await this.client.settings.bot.get('giveaways') || []
+    /* const db = await this.client.settings.giveaways.get(messageID) || []
     // Remove the giveaway from the array
     const newGiveawaysArray = db.filter((giveaway) => giveaway.messageID !== messageID)
-    // Save the updated array
-    await this.client.settings.bot.update('0', { giveaways: newGiveawaysArray })
+    // Save the updated array */
+    await this.client.settings.giveaways.delete(messageID)
     // Don't forget to return something!
     return true
   }
