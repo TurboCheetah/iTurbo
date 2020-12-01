5761f200421923b89579e92099e44ef5d46cb7b1
Francisco Ponce <me@turbo.ooo>
2020-12-01 13:10:50 -0500

    Test

--- commands/music/play.js
+++ commands/music/play.js
@@ -30,9 +30,15 @@ class Play extends Command {
     if (args[0].indexOf('open.spotify.com') > -1 || args[0].indexOf('play.spotify.com') > -1) {
       if (args[0].indexOf('/playlist/') > -1) {
         const data = await getTracks(args[0])
+        let songs = []
         data.forEach(song => {
-          this.client.distube.play(ctx.message, `${song.artists[0].name} - ${song.name}`)
+          const search = await ytsr(`${song.artists[0].name} - ${song.name}`, { limit: 1 })
+          const results = search.items.map(i => new SearchResult(i))
+          if (results.length === 0) throw Error('No result!')
+          return songs.push(results[0].url)
         })
+        console.log(songs);
+        return this.client.distube.playCustomPlaylist(ctx.message, songs)
       }
       const data = await getPreview(args[0])
       const search = await ytsr(`${data.artist} - ${data.title}`, { limit: 1 })
