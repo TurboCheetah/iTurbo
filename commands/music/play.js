5d902beda19a2c5a0023dd542d0d0a819dd89e3e
Francisco Ponce <me@turbo.ooo>
2020-12-08 12:00:11 -0500

    Spotify web API

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,8 +1,13 @@
 const Command = require('../../structures/Command.js')
 const { MessageEmbed } = require('discord.js')
-const { getPreview, getTracks } = require('spotify-url-info')
+// const { getPreview, getTracks } = require('spotify-url-info')
+const SpotifyWebApi = require('spotify-web-api-node')
 const ytsr = require('@distube/ytsr')
 const SearchResult = require('distube/src/SearchResult')
+const spotifyApi = new SpotifyWebApi({
+  clientId: this.client.config.spotify.id,
+  clientSecret: this.client.config.spotify.secret
+})
 
 class Play extends Command {
   constructor (...args) {
@@ -14,6 +19,71 @@ class Play extends Command {
       guildOnly: true,
       cost: 0
     })
+  }
+
+  async auth () {
+    const TOKEN = await spotifyApi.clientCredentialsGrant()
+    return spotifyApi.setAccessToken(TOKEN.body.access_token)
+  }
+
+  async getID (url) {
+    const URL = url.substring(url.search(/(album).|(track).|(playlist)./g), url.length)
+    return URL.substring(URL.search('/') + 1, URL.length)
+  }
+
+  async handleTrack (ctx, id) {
+    const artists = []
+    const data = await spotifyApi.getTrack(id).catch((err) => console.err(err))
+    data.body.artists.map((artist) => artists.push(artist.name))
+    const search = await ytsr(`${data.body.name} ${artists.join(', ')}`, { limit: 1 })
+    const results = search.items.map(i => new SearchResult(i))
+    if (results.length === 0) throw Error('No result!')
+    return this.client.distube.play(ctx.message, results[0].url)
+  }
+
+  async handleAlbum (ctx, id) {
+    const m = await ctx.reply('Please wait, adding songs to queue...')
+    const ids = []
+    const songs = []
+    const data = await spotifyApi.getAlbum(id).catch((err) => console.log(err))
+    data.body.tracks.items.map((e) => ids.push(e.id))
+    for (const id of ids) {
+      const artists = []
+      const data = await spotifyApi.getTrack(id).catch((err) => console.err(err))
+      data.body.artists.map((artist) => artists.push(artist.name))
+      const search = await ytsr(`${data.body.name} ${artists.join(', ')}`, { limit: 1 })
+      const results = search.items.map(i => new SearchResult(i))
+      if (results.length === 0) throw Error('No result!')
+      songs.push(results[0].url)
+    }
+
+    m.edit(`Added ${data.body.tracks.total} from ${data.body.name} - ${data.body.label} album`).then(ctx => {
+      ctx.delete({ timeout: 2500 })
+    })
+    return this.client.distube.playCustomPlaylist(ctx.message, songs, { name: data.body.label })
+  }
+
+  async handlePlaylist (ctx, id) {
+    const m = await ctx.reply('Please wait, adding songs to queue...')
+    const ids = []
+    const songs = []
+    const data = await spotifyApi.getPlaylist(id, { pageSize: 200, limit: 200 }).catch((err) => console.log(err))
+    data.body.tracks.items.map((e) => ids.push(e.id))
+    for (const id of ids) {
+      const artists = []
+      const data = await spotifyApi.getTrack(id).catch((err) => console.err(err))
+      data.body.artists.map((artist) => artists.push(artist.name))
+      const search = await ytsr(`${data.body.name} ${artists.join(', ')}`, { limit: 1 })
+      const results = search.items.map(i => new SearchResult(i))
+      if (results.length === 0) throw Error('No result!')
+      songs.push(results[0].url)
+    }
+
+    m.edit(`Added ${data.body.tracks.total} from **${data.body.name}** playlist by **${data.body.owner.display_name}**`).then(ctx => {
+      ctx.delete({ timeout: 2500 })
+    })
+
+    return this.client.distube.playCustomPlaylist(ctx.message, songs, { name: data.body.name })
   }
 
   async run (ctx, args) {
@@ -28,7 +98,7 @@ class Play extends Command {
     if (!args.length) return ctx.reply('What do you want me to play? Please provide a search query or song url!')
 
     if (args[0].indexOf('open.spotify.com' || 'play.spotify.com') > -1) {
-      if (args[0].indexOf('/playlist/') > -1) {
+      /*       if (args[0].indexOf('/playlist/') > -1) {
         const m = await ctx.reply('Please wait, adding songs to queue...')
         const data = await getTracks(args[0])
         const songs = []
@@ -50,7 +120,22 @@ class Play extends Command {
       const search = await ytsr(`${data.artist} - ${data.title}`, { limit: 1 })
       const results = search.items.map(i => new SearchResult(i))
       if (results.length === 0) throw Error('No result!')
-      return this.client.distube.play(ctx.message, results[0].url)
+      return this.client.distube.play(ctx.message, results[0].url) */
+      await this.auth()
+      const url = args[0]
+      const id = this.getID(url)
+      if (url.search('album') > 1) {
+        this.handleAlbum(ctx, id); return
+      }
+      if (url.search('track') > 1) {
+        this.handleTrack(id); return
+      }
+      if (url.search('playlist') > 1) {
+        this.handlePlaylist(ctx, id); return
+      }
+      ctx.reply('Invalid link').then(ctx => {
+        ctx.delete({ timeout: 2500 })
+      })
     }
 
     this.client.distube.play(ctx.message, args.join(' '))
