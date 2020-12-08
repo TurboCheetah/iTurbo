293b06a844fc395ea61f3ccc6b07b756c40f34b8
Francisco Ponce <me@turbo.ooo>
2020-12-08 13:51:03 -0500

    Proper Spotify API

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,6 +1,5 @@
 const Command = require('../../structures/Command.js')
 const { MessageEmbed } = require('discord.js')
-// const { getPreview, getTracks } = require('spotify-url-info')
 const ytsr = require('@distube/ytsr')
 const SearchResult = require('distube/src/SearchResult')
 
@@ -42,8 +41,8 @@ class Play extends Command {
     const songs = []
     const data = await this.client.spotifyApi.getAlbum(id).catch((err) => console.log(err))
     data.body.tracks.items.map((e) => ids.push(e.id))
+    const artists = []
     for (const id of ids) {
-      const artists = []
       const data = await this.client.spotifyApi.getTrack(id).catch((err) => console.error(err))
       data.body.artists.map((artist) => artists.push(artist.name))
       const search = await ytsr(`${data.body.name} ${artists.join(', ')}`, { limit: 1 })
@@ -52,7 +51,7 @@ class Play extends Command {
       songs.push(results[0].url)
     }
 
-    m.edit(`Added ${data.body.tracks.total} from ${data.body.name} - ${data.body.label} album`).then(ctx => {
+    m.edit(`Added ${data.body.tracks.total} from ${artists.join(' ')} - ${data.body.name} album`).then(ctx => {
       ctx.delete({ timeout: 2500 })
     })
     return this.client.distube.playCustomPlaylist(ctx.message, songs, { name: data.body.label })
@@ -92,29 +91,6 @@ class Play extends Command {
     if (!args.length) return ctx.reply('What do you want me to play? Please provide a search query or song url!')
 
     if (args[0].indexOf('open.spotify.com' || 'play.spotify.com') > -1) {
-      /*       if (args[0].indexOf('/playlist/') > -1) {
-        const m = await ctx.reply('Please wait, adding songs to queue...')
-        const data = await getTracks(args[0])
-        const songs = []
-        for (const song of data) {
-          if (!song || song === null || !song.artists || song.artists === null) {
-            break
-          }
-
-          const search = await ytsr(`${song.artists[0].name} - ${song.name}`, { limit: 1 })
-          const results = search.items.map(i => new SearchResult(i))
-          if (results.length === 0) throw Error('No result!')
-          songs.push(results[0].url)
-        }
-
-        m.delete()
-        return this.client.distube.playCustomPlaylist(ctx.message, songs)
-      }
-      const data = await getPreview(args[0])
-      const search = await ytsr(`${data.artist} - ${data.title}`, { limit: 1 })
-      const results = search.items.map(i => new SearchResult(i))
-      if (results.length === 0) throw Error('No result!')
-      return this.client.distube.play(ctx.message, results[0].url) */
       await this.auth()
       const url = args[0]
       const id = this.getID(url)
@@ -131,7 +107,6 @@ class Play extends Command {
         ctx.delete({ timeout: 2500 })
       })
     }
-
     this.client.distube.play(ctx.message, args.join(' '))
   }
 }
