87e47f087bc0654382cb8def56481cfff51bbfda
Turbo <turbocheetah.biz@gmail.com>
2020-12-18 00:43:34 -0500

    Delete Distube stuff

--- events/searchCancel.js
+++
@@ -1,8 +0,0 @@
-const Event = require('../structures/Event.js')
-class searchCancel extends Event {
-  async run (msg) {
-    msg.channel.send('Searching canceled')
-  }
-}
-
-module.exports = searchCancel
