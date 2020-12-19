0357f8c99ea941e45be13a5c19b80b7f25225ff8
Turbo <turbocheetah.biz@gmail.com>
2020-12-18 20:06:40 -0500

    Fix

--- events/addList.js
+++ events/addList.js
@@ -5,13 +5,13 @@ class addList extends Event {
   async run (ctx, player, res) {
     const channel = this.client.channels.cache.get(player.textChannel)
 
-    if (channel.guild.settings.nowplaying === false) return
+    if (player.queue.size > 1 && channel.guild.settings.nowplaying === false) return
 
     const embed = new MessageEmbed()
       .setColor(0x9590EE)
       .setAuthor(`Enqueued ${res.tracks ? res.tracks.length : res.songs.length} songs from playlist`, 'https://i.imgur.com/Nmg88HS.png')
       .setTitle(res.playlist ? res.playlist.name : res.name)
-      .setThumbnail(`https://img.youtube.com/vi/${res.playlist.songs[0].identifier}/maxresdefault.jpg`)
+      .setThumbnail(res.tracks ? `https://img.youtube.com/vi/${res.tracks[0].identifier}/maxresdefault.jpg` : `https://img.youtube.com/vi/${res.playlist.songs[0].identifier}/maxresdefault.jpg`)
       .addField('Requested by', res.tracks ? res.tracks[0].requester : ctx.author, true)
     if (res.playlist) embed.addField('Duration', this.client.utils.formatDuration(res.playlist.duration), true)
     channel.send({ embed }).then(ctx => ctx.delete({ timeout: 15000 }))
