efd1c60e7286f737c546dcff925d31f905e11b3e
Turbo <turbocheetah.biz@gmail.com>
2020-12-20 00:33:16 -0500

    Fix

--- events/addList.js
+++ events/addList.js
@@ -11,7 +11,7 @@ class addList extends Event {
       .setColor(0x9590EE)
       .setAuthor(`Enqueued ${res.tracks ? res.tracks.length : res.songs.length} songs from playlist`, 'https://i.imgur.com/Nmg88HS.png')
       .setTitle(res.playlist ? res.playlist.name : res.name)
-      .setThumbnail(res.tracks ? `https://img.youtube.com/vi/${res.tracks[0].identifier}/maxresdefault.jpg` : `https://img.youtube.com/vi/${res.playlist.songs[0].identifier}/maxresdefault.jpg`)
+      .setThumbnail(res.tracks ? `https://img.youtube.com/vi/${res.tracks[0].identifier}/maxresdefault.jpg` : `https://img.youtube.com/vi/${res.songs[0].identifier}/maxresdefault.jpg`)
       .addField('Requested by', res.tracks ? res.tracks[0].requester : ctx.author, true)
     if (res.playlist) embed.addField('Duration', this.client.utils.formatDuration(res.playlist.duration), true)
     channel.send({ embed }).then(ctx => ctx.delete({ timeout: 15000 }))
