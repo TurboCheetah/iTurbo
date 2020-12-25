dc4c147fb9a655f4ec99bd801d2b5caba39afadd
Turbo <turbocheetah.biz@gmail.com>
2020-12-24 23:50:50 -0500

    Invite URL & queue API

--- structures/web/app.js
+++ structures/web/app.js
@@ -22,13 +22,27 @@ module.exports = (client) => {
     res.redirect(301, 'https://turbo.ooo')
   })
 
+  app.get('/invite', (req, res) => {
+    res.redirect(301, 'https://discord.com/oauth2/authorize?client_id=175249503421464576&permissions=2016537702&scope=bot')
+  })
+
   app.get('/playlist/:userID/:playlistName', async (req, res) => {
     const id = req.params.userID
     const playlistName = req.params.playlistName
     const data = await client.settings.users.get(id).playlist[playlistName]
-    if (data === undefined) return res.status(404).json({ message: 'Not found' })
+    if (!data) return res.status(404).json({ message: 'Not found' })
     if (data.public === false) return res.status(403).json({ message: 'Forbidden' })
     res.json(data)
+  })
+
+  app.get('/player/:guildID/', async (req, res) => {
+    const id = req.params.guildID
+    if (!client.manager.players.get(id)) return res.status(404).json({ message: 'Guild is not playing music' })
+    const current = client.manager.players.get(id).queue.current
+    let queue = client.manager.players.get(id).queue
+    if (!current) return res.status(404).json({ message: 'Guild is not playing music' })
+    if (!queue) queue = {}
+    res.json({ current, queue })
   })
 
   app.use(middlewares.notFound)
