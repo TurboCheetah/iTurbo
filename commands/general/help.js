4e55c678ae3ae4171fa8b5c0eec9a770aebbdd3d
Turbo <turbocheetah.biz@gmail.com>
2021-01-07 09:02:48 -0500

    Paginate help command

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,5 +1,6 @@
 const Command = require('../../structures/Command.js')
 const { MessageEmbed } = require('discord.js')
+const { FieldsEmbed } = require('discord-paginationembed')
 
 class Help extends Command {
   constructor(...args) {
@@ -11,14 +12,6 @@ class Help extends Command {
   }
 
   async run(ctx, [command]) {
-    const splitCategory = (embedObj, text, maxLen = 1024) => {
-      if (text.length > maxLen) {
-        for (let fieldNum = 1; fieldNum < Math.ceil(text.length / 1024); fieldNum++) {
-          embedObj.addField('Continued', text.substr(text.substr(0, maxLen - 3).lastIndexOf(' ['), maxLen - 3))
-        }
-      }
-    }
-
     const map = {} // Map<Category, Array<Command.Name>>
     for (const command of this.store.values()) {
       // Check for hidden commands first so if all commands in a category is hidden we won't even show the category.
@@ -36,15 +29,23 @@ class Help extends Command {
     if (command) {
       if (keys.includes(this.client.utils.toProperCase(command))) {
         const category = this.client.utils.toProperCase(command)
-        const cmds = map[category].map(i => `[\`${i}\`](https://turbo.ooo '${this.store.get(i).description}${this.store.get(i).aliases.length > 0 ? `\nAliases: ${this.store.get(i).aliases.join(', ')}` : ''}')`).join(' ')
-        const embed = new MessageEmbed()
+
+        const Pagination = new FieldsEmbed()
+          .setArray(map[category])
+          .setAuthorizedUsers([ctx.author.id])
+          .setChannel(ctx.channel)
+          .setElementsPerPage(7)
+          .setPage(1)
+          .setPageIndicator('footer', (page, pages) => `Requested by ${ctx.author.tag} • Page ${page} of ${pages}`)
+          .formatField('Commands', i => `• [**${i}**](https://docs.iturbo.cc/commands/${category.toLowerCase()}#${i} '${this.store.get(i).aliases.length > 0 ? `\nAliases: ${this.store.get(i).aliases.join(', ')}` : ''}') - ${this.store.get(i).description}`)
+
+        Pagination.embed
           .setColor(0x9590ee)
           .setAuthor(`Help - ${category}`, this.client.user.displayAvatarURL({ size: 32 }))
-          .setDescription(`For more information about a command run \`${ctx.guild ? ctx.guild.settings.prefix : '|'}help <command>\``)
-          .addField('Commands', cmds.substr(0, cmds.substr(0, 1024 - 3).lastIndexOf(' [')))
-          .setFooter(`Requested by ${ctx.author.tag} • Hover over commands for info!`, ctx.author.displayAvatarURL({ size: 32 }))
-        splitCategory(embed, cmds)
-        return ctx.reply({ embed })
+          .setDescription(`For more information about a command run \`${ctx.guild ? ctx.guild.settings.prefix : '|'}help <command>\` or hover over it`)
+          .setFooter(`Requested by ${ctx.author.tag}`, ctx.author.displayAvatarURL({ size: 64 }))
+
+        return Pagination.build()
       }
 
       const cmd = this.store.get(command)
@@ -69,7 +70,8 @@ class Help extends Command {
 
       return ctx.reply(
         new MessageEmbed()
-          .setTitle(`Help - ${cmd.name}`)
+          .setTitle(`Help - ${this.client.utils.toProperCase(cmd.name)}`)
+          .setURL(`https://docs.iturbo.cc/commands/${cmd.category.toLowerCase()}#${cmd.name}`)
           .setColor(0x9590ee)
           .setAuthor(this.client.user.tag, this.client.user.displayAvatarURL({ size: 64 }))
           .setDescription([`**Description:** ${cmd.description}`, `**Category:** ${cmd.category}`, `**Aliases:** ${cmd.aliases.length ? cmd.aliases.join(', ') : 'None'}`, `**Cooldown:** ${cmd.cooldown ? cmd.cooldown + ' Seconds' : 'None'}`, `**Usage:** ${ctx.guild ? ctx.guild.settings.prefix : '|'}${cmd.usage}`, `**Cost:** ${cost}`, `**Extended Help:** ${cmd.extendedHelp}`].join('\n'))
@@ -78,10 +80,11 @@ class Help extends Command {
 
     const embed = new MessageEmbed()
       .setColor(0x9590ee)
-      // .setAuthor(this.client.user.tag, this.client.user.displayAvatarURL({ size: 64 }))
-      .setAuthor('Help', this.client.user.displayAvatarURL({ size: 64 }))
+      .setAuthor(this.client.user.tag, this.client.user.displayAvatarURL({ size: 64 }))
+      .setTitle('Help')
+      .setURL('https://docs.iturbo.cc')
       .setDescription(`For all commands in a category run \`${ctx.guild ? ctx.guild.settings.prefix : '|'}help <category>\`\nIf you need further help feel free to join the [support server](https://discord.gg/011UYuval0uSxjmuQ).`)
-      .addField('Available Categories', keys.map(key => `\`${key}\``).join(' '))
+      .addField('Available Categories', keys.map(key => `[\`${key}\`](https://docs.iturbo.cc/commands/${key.toLowerCase()})`).join(' '))
       .setImage('https://i.imgur.com/g3jV9fg.gif')
       .setFooter(`Requested by ${ctx.author.tag}`, ctx.author.displayAvatarURL({ size: 32 }))
 
