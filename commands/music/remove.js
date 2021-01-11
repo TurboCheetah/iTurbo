20137fb786b9dc4bf54a185de5c344dcaec8b946
Turbo <turbocheetah.biz@gmail.com>
2021-01-11 17:15:58 -0500

    Remove command

---
+++ commands/music/remove.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-case-declarations */
+const Command = require('../../structures/Command.js')
+const { MessageEmbed } = require('discord.js')
+
+class Remove extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: 'Removes the desired song from the queue',
+      botPermissions: ['CONNECT', 'SPEAK', 'EMBED_LINKS'],
+      usage: 'remove <songPosition>',
+      guildOnly: true,
+      cost: 0,
+      cooldown: 3
+    })
+  }
+
+  async run(ctx, [songPosition]) {
+    const djRole = ctx.guild.settings.djRole
+
+    if (djRole) {
+      if (!ctx.member.roles.cache.has(djRole) && !ctx.member.permissions.has('MANAGE_GUILD')) return ctx.reply(`${this.client.constants.error} You are not a DJ! You need the ${ctx.guild.roles.cache.find(r => r.id === djRole)} role!`)
+    }
+
+    const channel = ctx.member.voice.channel
+
+    if (!channel) return ctx.reply(`${this.client.constants.error} You need to be in a voice channel to remove music from the queue!`)
+
+    const player = this.client.manager.players.get(ctx.guild.id)
+
+    if (!player || !player.queue.length > 0) {
+      const embed = new MessageEmbed().setColor(0x9590ee).setAuthor('| There is nothing to move!', ctx.author.displayAvatarURL({ size: 512 }))
+      return ctx.reply({ embed })
+    }
+
+    if (!songPosition) return ctx.reply(`${this.client.constants.error} Correct usage: \`${ctx.guild.settings.prefix}move <songPosition> <newPosition>\`\nExample: \`${ctx.guild.settings.prefix}move 3 1\``)
+
+    songPosition = this.verifyInt(songPosition, 1) - 2
+
+    ctx.reply(`${this.client.constants.success} Removed **${player.queue[songPosition].title}** from the queue!`)
+
+    player.queue.remove(songPosition)
+  }
+}
+
+module.exports = Remove
