03d35ff87bb58cb70a97837b5e36054ac6682ab0
Turbo <turbocheetah.biz@gmail.com>
2021-01-12 10:57:10 -0500

    Fix error if in dev environment

--- events/commandError.js
+++ events/commandError.js
@@ -6,7 +6,7 @@ class CommandError extends Event {
     // Allow `throw "Error message"` to rewind stack and reply from nested calls from a command.
     if (typeof err === 'string') return ctx.reply(err)
     console.log(`[COMMAND] ${ctx.command.name}: ${err.stack || err}`)
-    this.client.sentry.captureException(err)
+    if (this.client.sentry) this.client.sentry.captureException(err)
     await ctx.reply(`${this.client.constants.error} Uh! Something went wrong unexpectedly!${ctx.author.id !== this.client.constants.ownerID ? " Don't worry my master will keep track of the problem and fix it soon." : ''}`)
     const channel = this.client.channels.cache.get('735638949770559569')
     if (!channel) return
