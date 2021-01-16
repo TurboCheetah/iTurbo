8a39f3cf871bd45db6064e8589f7c65781fd8a75
Turbo <turbocheetah.biz@gmail.com>
2021-01-16 02:25:05 -0500

    feat: improve help command
    * Better UI
    * Add arguments and examples

--- commands/general/help.js
+++ commands/general/help.js
@@ -36,19 +36,19 @@ class Help extends Command {
           .setChannel(ctx.channel)
           .setElementsPerPage(7)
           .setPage(1)
-          .setPageIndicator('footer', (page, pages) => `Requested by ${ctx.author.tag} • Page ${page} of ${pages}`)
+          .setPageIndicator('footer', (page, pages) => `Page ${page} of ${pages}`)
           .formatField('Commands', i => `• [**${i}**](https://docs.iturbo.cc/commands/${category.toLowerCase()}#${i} '${this.store.get(i).aliases.length > 0 ? `\nAliases: ${this.store.get(i).aliases.join(', ')}` : ''}') - ${this.store.get(i).description}`)
 
         Pagination.embed
           .setColor(0x9590ee)
           .setAuthor(`Help - ${category}`, this.client.user.displayAvatarURL({ size: 32, dynamic: true }))
           .setDescription(`For more information about a command run \`${ctx.guild ? ctx.guild.settings.prefix : '|'}help <command>\` or hover over it`)
-          .setFooter(`Requested by ${ctx.author.tag}`, ctx.author.displayAvatarURL({ size: 64, dynamic: true }))
+          .setFooter(`Requested by ${ctx.author.tag}`, null)
 
         return Pagination.build()
       }
 
-      const cmd = this.store.get(command)
+      const cmd = this.store.get(command.toLowerCase())
       if (!cmd) return ctx.reply("That category/command does not exist! Why would you think I'd have such a thing?")
 
       let cost = 'Free'
@@ -68,14 +68,27 @@ class Help extends Command {
         return ctx.reply("You can't view details of that command in a non NSFW channel.")
       }
 
-      return ctx.reply(
-        new MessageEmbed()
-          .setTitle(`Help - ${this.client.utils.toProperCase(cmd.name)}`)
-          .setURL(`https://docs.iturbo.cc/commands/${cmd.category.toLowerCase()}#${cmd.name}`)
-          .setColor(0x9590ee)
-          .setAuthor(this.client.user.tag, this.client.user.displayAvatarURL({ size: 64, dynamic: true }))
-          .setDescription([`**Description:** ${cmd.description}`, `**Category:** ${cmd.category}`, `**Aliases:** ${cmd.aliases.length ? cmd.aliases.join(', ') : 'None'}`, `**Cooldown:** ${cmd.cooldown ? cmd.cooldown + ' Seconds' : 'None'}`, `**Usage:** ${ctx.guild ? ctx.guild.settings.prefix : '|'}${cmd.usage}`, `**Cost:** ${cost}`, `**Extended Help:** ${cmd.extendedHelp}`].join('\n'))
-      )
+      const cmdArgs = []
+      for (const [key, value] of Object.entries(cmd.arguments)) cmdArgs.push(`• \`${key}\` ${value}`)
+      const cmdExamples = []
+      for (const [key, value] of Object.entries(cmd.examples)) cmdExamples.push(`• \`${ctx.guild ? ctx.guild.settings.prefix : '|'}${cmd.name} ${key}\` ${value}`)
+
+      const embed = new MessageEmbed()
+        .setTitle(`Help - ${this.client.utils.toProperCase(cmd.name)}`)
+        .setURL(`https://docs.iturbo.cc/commands/${cmd.category.toLowerCase()}#${cmd.name}`)
+        .setColor(0x9590ee)
+        .addField('Usage', `${ctx.guild ? ctx.guild.settings.prefix : '|'}${cmd.usage}`)
+        .addField('Description', cmd.description)
+        .addField('Aliases', cmd.aliases.length ? cmd.aliases.join(', ') : 'None')
+      if (cmdArgs.length) embed.addField('Arguments', cmdArgs.join('\n'))
+      if (cmdExamples.length) embed.addField('Examples', cmdExamples.join('\n'))
+      // eslint-disable-next-line prettier/prettier
+      embed.addField('Category', cmd.category)
+        .setFooter(`Cost: ${cost} • Cooldown: ${cmd.cooldown ? `${cmd.cooldown} Seconds` : 'None'}`)
+
+      if (cmd.extendedHelp !== 'No extended help provided.') embed.addField('Extended Help', cmd.extendedHelp)
+
+      return ctx.reply({ embed })
     }
 
     const embed = new MessageEmbed()
@@ -86,7 +99,6 @@ class Help extends Command {
       .setDescription(`For all commands in a category run \`${ctx.guild ? ctx.guild.settings.prefix : '|'}help <category>\`\nIf you need further help feel free to join the [support server](https://discord.gg/011UYuval0uSxjmuQ).`)
       .addField('Available Categories', keys.map(key => `[\`${key}\`](https://docs.iturbo.cc/commands/${key.toLowerCase()})`).join(' '))
       .setImage('https://i.imgur.com/g3jV9fg.gif')
-      .setFooter(`Requested by ${ctx.author.tag}`, ctx.author.displayAvatarURL({ size: 32, dynamic: true }))
 
     /*     for (const category of keys) {
           // Skip un-needed categories
