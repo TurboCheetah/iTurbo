c48212bf6b069535782379b7de08f9efb37afd9e
Turbo <turbocheetah.biz@gmail.com>
2021-01-15 20:25:15 -0500

    feat: add npNotify

--- commands/config/settings.js
+++ commands/config/settings.js
@@ -29,13 +29,17 @@ class Settings extends Command {
       .setTitle('Guild settings')
       .setColor(0x9590ee)
       .addField('Prefix', `\`${ctx.guild.settings.prefix}\``, true)
+      .addField(this.client.constants.zws, this.client.constants.zws, true)
       .addField('Level Up Messages', ctx.guild.settings.levelup ? 'Enabled' : 'Disabled', true)
       .addField('Economy System', ctx.guild.settings.social ? 'Enabled' : 'Disabled', true)
+      .addField(this.client.constants.zws, this.client.constants.zws, true)
       .addField('Starboard', ctx.guild.settings.starboard ? ctx.guild.channels.cache.find(c => c.id === ctx.guild.settings.starboard) : 'Disabled', true)
       .addField('Weeb Greetings', ctx.guild.settings.weebGreetings ? ctx.guild.channels.cache.find(c => c.id === ctx.guild.settings.weebGreetings) : 'Disabled', true)
+      .addField(this.client.constants.zws, this.client.constants.zws, true)
       .addField('DJ Role', ctx.guild.settings.djRole ? ctx.guild.roles.cache.find(r => r.id === ctx.guild.settings.djRole) : 'Disabled', true)
       .setFooter(`Requested by: ${ctx.author.tag}`, ctx.author.displayAvatarURL({ size: 64, dynamic: true }))
 
+    if (!ctx.guild.settings.nowplaying) embed.addField('Now Playing Notifications', 'Disabled', true)
     // eslint-disable-next-line prettier/prettier
     if (ctx.guild.settings.social && (ctx.guild.settings.disabledChannels && ctx.guild.settings.disabledChannels.length > 0)) embed.addField('Disabled Level Up Channels', ctx.guild.settings.disabledChannels.map(channel => ctx.guild.channels.cache.find(c => c.id === channel)), false)
     ctx.reply({ embed })
