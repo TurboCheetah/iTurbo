2e94aab4e98a005750fef60e6bc690955175a6da
Turbo <turbocheetah.biz@gmail.com>
2021-01-28 14:06:07 -0500

    fix: unmute permissions

--- commands/moderation/unmute.js
+++ commands/moderation/unmute.js
@@ -6,7 +6,7 @@ class Unmute extends Command {
     super(...args, {
       description: 'Unmutes a user.',
       userPermissions: ['KICK_MEMBERS'],
-      botPermissions: ['KICK_MEMBERS', 'EMBED_LINKS'],
+      botPermissions: ['KICK_MEMBERS', 'EMBED_LINKS', 'MANAGE_ROLES'],
       guildOnly: true,
       usage: 'unmute <@member> [reason]'
     })
