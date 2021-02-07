c37d0a88cf156a94d5639dff2726b51baf6ee46b
Turbo <turbocheetah.biz@gmail.com>
2021-02-06 19:50:50 -0500

    fix: server errors not in JSON causing error

--- commands/programming/npmpkgsize.js
+++ commands/programming/npmpkgsize.js
@@ -16,18 +16,22 @@ class NPMPkgSize extends Command {
   async run(ctx, [name]) {
     if (!name) return ctx.reply('What package am I supposed to show you?')
 
-    const { publishSize, installSize } = await c(`https://packagephobia.now.sh/api.json?p=${encodeURIComponent(name)}`).json()
+    try {
+      const { publishSize, installSize } = await c(`https://packagephobia.now.sh/api.json?p=${encodeURIComponent(name)}`).json()
 
-    if (!publishSize && !installSize) return ctx.reply("That package doesn't exist.")
+      if (!publishSize && !installSize) return ctx.reply("That package doesn't exist.")
 
-    const embed = new MessageEmbed()
-      .setTitle(`NPM Package Size - ${name}`)
-      .setColor(0x9590ee)
-      .setDescription(`**Publish Size:** ${this.client.utils.getBytes(publishSize)}\n**Install Size:** ${this.client.utils.getBytes(installSize)}`)
-      .setFooter('Powered by packagephobia.now.sh')
-      .setAuthor(ctx.author.tag, ctx.author.displayAvatarURL({ size: 64, dynamic: true }))
+      const embed = new MessageEmbed()
+        .setTitle(`NPM Package Size - ${name}`)
+        .setColor(0x9590ee)
+        .setDescription(`**Publish Size:** ${this.client.utils.getBytes(publishSize)}\n**Install Size:** ${this.client.utils.getBytes(installSize)}`)
+        .setFooter('Powered by packagephobia.now.sh')
+        .setAuthor(ctx.author.tag, ctx.author.displayAvatarURL({ size: 64, dynamic: true }))
 
-    return ctx.reply({ embed })
+      return ctx.reply({ embed })
+    } catch (err) {
+      throw "That package doesn't exist."
+    }
   }
 }
 
