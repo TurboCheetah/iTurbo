8e997f292cde334be66bbbb124af1271737fc0b1
Turbo <turbocheetah.biz@gmail.com>
2021-05-17 15:53:36 -0500

    fix: remove unneccesary arrow function

--- utils/Utils.js
+++ utils/Utils.js
@@ -143,19 +143,19 @@ class Utils {
     return `https://haste.turbo.ooo/${key}${lang ? `.${lang}` : ''}`
   }
 
-  static createBar = (total, current, size = 40, line = '▬', slider = '🔘', url) => {
-    if (!total) throw new Error('Total value is either not provided or invalid');
-    if (!current && current !== 0) throw new Error('Current value is either not provided or invalid');
-    if (isNaN(total)) throw new Error('Total value is not an integer');
-    if (isNaN(current)) throw new Error('Current value is not an integer');
-    if (isNaN(size)) throw new Error('Size is not an integer');
+  static createBar(total, current, size = 40, line = '▬', slider = '🔘', url) {
+    if (!total) throw new Error('Total value is either not provided or invalid')
+    if (!current && current !== 0) throw new Error('Current value is either not provided or invalid')
+    if (isNaN(total)) throw new Error('Total value is not an integer')
+    if (isNaN(current)) throw new Error('Current value is not an integer')
+    if (isNaN(size)) throw new Error('Size is not an integer')
     if (current > total) {
       const bar = line.repeat(size + 2)
       const percentage = (current / total) * 100
       return [bar, percentage]
     } else {
       const percentage = current / total
-      const progress = Math.round((size * percentage))
+      const progress = Math.round(size * percentage)
       const emptyProgress = size - progress
       const progressText = `[${line.repeat(progress).replace(/.$/, slider)}](${url} '${this.formatDuration(current)}')`
       const emptyProgressText = line.repeat(emptyProgress)
