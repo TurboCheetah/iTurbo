cbf4ea2f21ad4dc0177cdafcaae49ef4d85f3ff6
Turbo <turbocheetah.biz@gmail.com>
2021-05-17 16:15:17 -0500

    feat: change status while rebooting

--- commands/owner/reboot.js
+++ commands/owner/reboot.js
@@ -10,9 +10,9 @@ class Reboot extends Command {
     })
   }
 
-  async run(ctx, args) {
-    // eslint-disable-line no-unused-vars
+  async run(ctx) {
     await ctx.reply('Shutting down...')
+    await this.client.user.setPresence({ activities: [{ name: 'myself reboot...', type: 'WATCHING' }], status: 'idle' })
     await this.client.dbconn.release()
     await this.client.db.end()
     process.exit()
