9d5744615a48f45fc4f6ec80855b112ee274078d
Turbo <turbocheetah.biz@gmail.com>
2021-05-21 18:19:13 -0500

    refactor: simplify Reddit commands

--- commands/nsfw/reddit.js
+++ commands/nsfw/reddit.js
@@ -1,151 +1,34 @@
 const Command = require('#structures/Command')
 const { MessageEmbed } = require('discord.js')
-const c = require('@aero/centra')
 
 class Reddit extends Command {
   constructor(...args) {
     super(...args, {
-      description: 'Returns a reddit post for the specified category. If no category is given, a post will be returned from any of the possible categories.',
-      usage: 'reddit',
-      extendedHelp: 'reddit [ass|boobs|thighs]',
+      description: language => language.get('redditDescription'),
+      usage: language => language.get('redditUsage'),
       aliases: ['porn', 'nsfwr', 'rnsfw'],
       cooldown: 3,
-      cost: 3,
+      cost: 15,
       nsfw: true,
       botPermissions: ['EMBED_LINKS']
     })
-
-    this.errorMessage = 'There was an error. Reddit may be down, or the subreddit doesnt exist.'
   }
 
-  async run(ctx, [args]) {
-    const client = this.client
-    let subreddits = []
-    switch (args) {
-      case 'ass':
-        subreddits = ['ass', 'paag', 'asstastic', 'buttplug', 'whooties', 'AssholeBehindThong', 'Frogbutt', 'rearpussy', 'CuteLittleButts', 'HungryButts', 'reversecowgirl', 'facedownassup', 'butt', 'butts', 'pawg', 'bigasses', 'cosplaybutts', 'girlsinyogapants', 'BubbleButts', 'assinthong', 'smalltitsbigass', 'CelebrityButts', 'booty']
-        break
+  async run(ctx, [type]) {
+    const subs = this.client.constants.subreddits
+    if (!type || !['ass', 'boobs', 'thighs'].includes(type)) type = 'all'
 
-      case 'boobs':
-        subreddits = ['smallboobs', 'boobs', 'tits', 'tinytits', 'bigtitssmallnip', 'boobies', 'rosynips', 'tiddies']
-        break
+    const { url, post: { title, subreddit, link, upvotes, author, comments } } = await this.client.ksoft.images.reddit(this.client.utils.random(type === 'all' ? [...subs.ass, ...subs.boobs, ...subs.thighs] : subs[type]), { span: 'month' })
 
-      case 'thighs':
-        subreddits = ['girlsinyogapants', 'Thighs', 'thighhighs', 'ThickThighs', 'UnderwearGW', 'datgap', 'leggingsgonewild', 'pawg', 'hipcleavage', 'legs', 'pantyhose']
-        break
+    const embed = new MessageEmbed()
+      .setColor(this.client.constants.color)
+      .setAuthor(author, undefined, `https://reddit.com${author}`)
+      .setTitle(title)
+      .setURL(link)
+      .setImage(url)
+      .setFooter(`${subreddit} • 👍 ${upvotes} • 💬 ${comments}`)
 
-      default:
-        subreddits = ['girlsinyogapants', 'Thighs', 'thighhighs', 'ThickThighs', 'UnderwearGW', 'datgap', 'leggingsgonewild', 'pawg', 'hipcleavage', 'legs', 'pantyhose', 'ass', 'paag', 'asstastic', 'buttplug', 'whooties', 'AssholeBehindThong', 'Frogbutt', 'rearpussy', 'CuteLittleButts', 'HungryButts', 'reversecowgirl', 'facedownassup', 'butt', 'butts', 'pawg', 'bigasses', 'cosplaybutts', 'BubbleButts', 'assinthong', 'smalltitsbigass', 'booty', 'panties', 'FullBackPanties', 'PantiesToTheSide', 'thongs', 'xsmallgirls', 'PublicSexPorn', 'cameltoe', 'smallboobs', 'LegalTeens', 'TooCuteForPorn', 'adorableporn', 'AsiansGoneWild', 'trashyboners', 'StraightGirlsPlaying', 'LipsThatGrip', 'spreadeagle', 'dirtysmall', 'nsfw', 'pussy', 'gonewild', 'SexyTummies', 'SpreadEm', 'Ahegao_IRL', 'nsfwcosplay', 'RealGirls', 'lesbians', 'Fingering', 'AnalGW', 'anal', 'freeuse', 'BorednIgnored', 'grool', 'jilling', 'porn', 'Amateur', 'TinyTits', 'PetiteGoneWild', 'cumsluts', 'AsianHotties', 'simps', 'slimgirls', 'ginger', 'palegirls', 'BustyPetite', 'Innie', 'boobs', 'tits', 'bigtitssmallnip', 'assvsboobs', 'boobies', 'fantastictits', 'onoff', 'fortyfivefiftyfive', 'juicyasians', 'Nude_Selfie', 'nude_snapchat', 'rosynips', 'tiddies', 'justhotwomen', 'nsfwverifiedamateurs', 'public', 'slut', 'sluts']
-        break
-    }
-
-    const site = client.utils.random(subreddits)
-    function request(site) {
-      return new Promise(function (resolve, reject) {
-        // eslint-disable-next-line prefer-promise-reject-errors
-        if (!site) reject({ reason: 'No subreddit supplied', message: "Couldn't make request because there wasn't a subreddit" })
-        function ExtractRedditUrl(body, tries) {
-          // eslint-disable-next-line prefer-promise-reject-errors
-          if (tries >= 25) return reject({ reason: 'Retry limit exceeded', message: 'Failed to find a suitable post', subbredit: site })
-          tries++
-          // grabs a random post
-          const post = client.utils.random(body).data
-          // checks if the post url ends with an image extension
-          switch (/(\.jpg|\.png|\.gif|\.jpeg)$/gi.test(post.url)) {
-            case true: {
-              // resolves the payload with all the juicy data
-              const payload = {
-                url: post.url,
-                subreddit: post.subreddit,
-                title: post.title,
-                source: post.permalink,
-                upvotes: post.ups,
-                comments: post.num_comments,
-                tries: tries
-              }
-              resolve(payload)
-              break
-            }
-            default:
-              // self explanatory (hopefully)
-              switch (post.is_video) {
-                case true:
-                  // tries to get another post if it's a video (this was used for discord and we can't embed videos)
-                  ExtractRedditUrl(body, tries)
-                  break
-                default:
-                  switch (post.media) {
-                    case null:
-                      // if media is null try again
-                      ExtractRedditUrl(body, tries)
-                      break
-                    default:
-                      // if the media thumbnail is from gfycat try again (thumbnails from gfycat are really low res)
-                      switch (post.url.includes('redgifs')) {
-                        case false: {
-                          // resolve payload
-                          const payload = {
-                            url: post.url,
-                            subreddit: post.subreddit,
-                            title: post.title,
-                            source: post.permalink,
-                            upvotes: post.ups,
-                            comments: post.num_comments,
-                            tries: tries
-                          }
-                          resolve(payload)
-                          break
-                        }
-                        // tries again
-                        default:
-                          ExtractRedditUrl(body, tries)
-                      }
-                      break
-                  }
-                  break
-              }
-              break
-          }
-        }
-        // just some randomness
-        const sortBy = ['best', 'top', 'hot']
-        const url = `https://www.reddit.com/r/${site}/${client.utils.random(sortBy)}.json?limit=15`
-        c(url)
-          .json()
-          .then(async response => {
-            try {
-              ExtractRedditUrl(response.data.children, 0)
-            } catch (error) {
-              reject(error)
-            }
-          })
-          .catch(error => {
-            // if the request fails reject
-            reject(error)
-          })
-      })
-    }
-
-    const getPorn = () => {
-      return request(site)
-    }
-
-    getPorn()
-      .then(r => {
-        const embed = new MessageEmbed()
-          .setTitle(`r/${r.subreddit} - ${r.title}`)
-          .setURL(`https://reddit.com${r.source}`)
-          .setColor(0x9590ee)
-          .setImage(r.url)
-          .setDescription(`:thumbsup: ${r.upvotes} | :speech_balloon: ${r.comments}`)
-          .setFooter('Powered by Reddit', ctx.author.displayAvatarURL({ size: 32, dynamic: true }))
-
-        return ctx.reply({ embed })
-      })
-      .catch(err => {
-        console.error(err)
-        ctx.reply(`Error: ${typeof err === 'object' ? (err = `Reason: ${err.reason}. ${err.message}.`) : err}`)
-      })
+    ctx.reply({ embed })
   }
 }
 
--- commands/nsfw/redditgif.js
+++ commands/nsfw/redditgif.js
@@ -1,49 +1,30 @@
 const Command = require('#structures/Command')
-const c = require('@aero/centra')
+const { MessageEmbed } = require('discord.js')
 
 class RedditGif extends Command {
   constructor(...args) {
     super(...args, {
-      description: 'Returns a random reddit post.',
-      usage: 'redditgif',
+      description: language => language.get('redditGifDescription'),
       aliases: ['porngif', 'pgif', 'nsfwrgif', 'rnsfwgif', 'rgif'],
       cooldown: 3,
-      cost: 3,
+      cost: 15,
       nsfw: true,
       botPermissions: ['EMBED_LINKS']
     })
-
-    this.errorMessage = 'There was an error. Reddit may be down, or the subreddit doesnt exist.'
   }
 
   async run(ctx) {
-    const subreddits = ['nsfw_gif', 'nsfw_gifs', 'porn_gifs', 'povjiggle', 'slowmojiggles', 'tittydrop', 'verticalgifs']
-    const data = await c(`https://www.reddit.com/r/${this.client.utils.random(subreddits)}.json?limit=800&?sort=hot&t=all`)
-      .json()
-      .then(body => {
-        if (body.error) throw this.errorMessage
-        return body.data.children
-      })
-      .catch(() => {
-        throw this.errorMessage
-      })
+    const { url, post: { title, subreddit, link, upvotes, author, comments } } = await this.client.ksoft.images.reddit(this.client.utils.random(this.client.constants.subreddits.gifs), { span: 'month' })
 
-    const nsfwPost = this.client.utils.random(data).data
+    const embed = new MessageEmbed()
+      .setColor(this.client.constants.color)
+      .setAuthor(author, undefined, `https://reddit.com${author}`)
+      .setTitle(title)
+      .setURL(link)
+      .setFooter(`${subreddit} • 👍 ${upvotes} • 💬 ${comments}`)
 
-    if (nsfwPost.over_18 && !ctx.channel.nsfw) {
-      return ctx.reply('The result I found was NSFW and I cannot post it in this channel.')
-    }
-
-    // Broken due to RedGifs
-    /*     const embed = new MessageEmbed()
-    .setTitle(`r/${nsfwPost.subreddit} - ${nsfwPost.title}`)
-    .setURL(`https://www.reddit.com/${nsfwPost.permalink}`)
-    .setColor(0x9590EE)
-    .setImage(nsfwPost.url)
-    .setFooter(`Powered by Reddit`, ctx.author.displayAvatarURL({ size: 32, dynamic: true }));
-
-    return ctx.reply({ embed }); */
-    return ctx.reply(`r/${nsfwPost.subreddit} - ${nsfwPost.title}\n${nsfwPost.url}`)
+    await ctx.reply({ embed })
+    await ctx.reply(url)
   }
 }
 
