b6dd6185c321794b8b4043f51af9a8471fe549aa
turbo <me@turbo.ooo>
2021-11-09 20:17:20 -0500

    feat: handle unhandledRejections

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import * as dotenv from 'dotenv'
 import path from 'path'
 import { Bot } from './Client'
+import { Logger } from './utils/Logger'
 
 dotenv.config({ path: path.join(__dirname, '/../.env') })
 process.env.NODE_ENV ??= 'development'
+process.on('unhandledRejection', (err: Error) => Logger.error(err))
 
 const bot = new Bot()
 bot.init()
