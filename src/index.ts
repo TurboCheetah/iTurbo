fdd8e83be10c80fc60e1e21c7a1c58aa91dd47e3
turbo <me@turbo.ooo>
2021-11-11 23:50:06 -0500

    feat: change logger to console.error

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import * as dotenv from 'dotenv'
 import { join } from 'path'
 import { ShardingManager } from 'kurasuta'
-import { Logger } from './utils/Logger'
 import { Bot } from './Client'
 import { Client } from 'discord.js'
 
 dotenv.config({ path: join(__dirname, '/../.env') })
 process.env.NODE_ENV ??= 'development'
-process.on('unhandledRejection', (err: Error) => Logger.error(err))
+process.on('unhandledRejection', (err: Error) => console.error(err))
 
 const manager = new ShardingManager(join(__dirname, 'Manager'), {
   development: process.env.NODE_ENV === 'development',
