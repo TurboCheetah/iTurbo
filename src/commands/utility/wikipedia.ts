20b17daf97aaec0df49b796d918030f3c5905c6d
turbo <me@turbo.ooo>
2021-11-17 17:46:51 -0500

    feat: wikipedia command

---
+++ src/commands/utility/wikipedia.ts
@@ -0,0 +1,25 @@
+import c from '@aero/centra'
+import { CommandInteraction, MessageEmbed } from 'discord.js'
+import { Discord, Slash, SlashOption } from 'discordx'
+import { toProperCase } from '../../utils/utils'
+
+@Discord()
+export abstract class WikipediaCommand {
+  @Slash('wikipedia', { description: 'Search for something on Wikipedia' })
+  async wikipedia(
+    @SlashOption('query', { description: "What you'd like to search for", required: true })
+    query: string,
+    @SlashOption('public', { description: 'Display this command publicly', required: false })
+    ephemeral: boolean,
+    interaction: CommandInteraction
+  ): Promise<any> {
+    await interaction.deferReply({ ephemeral: !ephemeral })
+
+    const article = await c('https://en.wikipedia.org/api/rest_v1/page/summary/', 'GET').path(toProperCase(query)).json()
+
+    if (!article.content_urls) interaction.editReply("I couldn't find a wikipedia article with that title!")
+
+    const embed = new MessageEmbed().setColor(0x9590ee).setThumbnail('https://i.imgur.com/fnhlGh5.png').setURL(article.content_urls.desktop.page).setTitle(article.title).setDescription(article.extract)
+    interaction.editReply({ embeds: [embed] })
+  }
+}
