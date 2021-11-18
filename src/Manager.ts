8c948042707b9071156d4459844661e6d7b3dd23
turbo <me@turbo.ooo>
2021-11-18 17:27:34 -0500

    do weird shit

--- src/Manager.ts
+++ src/Manager.ts
@@ -1,16 +1,11 @@
-import { BaseCluster, ShardingManager } from 'kurasuta'
-import { IslaClient } from '#/Client'
+import { importx } from '@discordx/importer'
+import { BaseCluster } from 'kurasuta'
+import path from 'path'
 
 export default class Manager extends BaseCluster {
-  public client!: IslaClient
-
-  constructor(...args: [ShardingManager]) {
-    super(...args)
-
-    this.client.cluster = this
-  }
-
   async launch(): Promise<void> {
-    this.client.login(process.env.NODE_ENV === 'development' ? process.env.DEV_TOKEN ?? '' : process.env.TOKEN ?? '')
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    await importx(path.join(__dirname, '/{events,commands}/**/*.{ts,js}'))
+    await this.client.login(process.env.NODE_ENV === 'development' ? process.env.DEV_TOKEN ?? '' : process.env.TOKEN ?? '')
   }
 }
