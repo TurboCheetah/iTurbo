d4498b4aca76e8b40278f71517486816fb062eb4
codefactor-io <support@codefactor.io>
2021-11-18 01:28:14 +0000

    [CodeFactor] Apply fixes

--- src/commands/reactions/hug.ts
+++ src/commands/reactions/hug.ts
@@ -12,7 +12,7 @@ export abstract class HugCommand {
     interaction: CommandInteraction,
     client: IslaClient
   ): Promise<void> {
-    if (member === (interaction.member as GuildMember)) return await interaction.reply({ content: "You can't hug yourself!", ephemeral: true })
+    if (member === (interaction.member as GuildMember)) return interaction.reply({ content: "You can't hug yourself!", ephemeral: true })
 
     await interaction.deferReply()
 
