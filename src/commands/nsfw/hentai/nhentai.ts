744fac69a97f8e380e8619d5b2ed1073c005e28a
turbo <me@turbo.ooo>
2022-01-06 00:12:36 -0500

    feat: clean up nHentai command

--- src/commands/nsfw/hentai/nhentai.ts
+++ src/commands/nsfw/hentai/nhentai.ts
@@ -26,7 +26,7 @@ export abstract class NHentaiCommands {
 
         if (doujin === null) return interaction.editReply({ embeds: [new MessageEmbed().setColor(0xee9090).setTitle('Doujin not found').setDescription('Please try another ID.')] })
 
-        const embeds = this.embedBuilder(doujin)
+        const embeds = this.embedBuilder(doujin, client)
 
         const pagination = new Pagination(interaction, embeds, { type: 'BUTTON', initialPage: page })
         await pagination.send()
@@ -43,13 +43,14 @@ export abstract class NHentaiCommands {
         await interaction.deferReply({ ephemeral: !ephemeral })
 
         const doujin = await client.nhentai.randomDoujin()
-        const embeds = this.embedBuilder(doujin)
+
+        const embeds = this.embedBuilder(doujin, client)
 
         const pagination = new Pagination(interaction, embeds, { type: 'BUTTON' })
         await pagination.send()
     }
 
-    embedBuilder(doujin: Doujin): MessageEmbed[] {
+    embedBuilder(doujin: Doujin, client: IslaClient): MessageEmbed[] {
         const coverEmbed = new MessageEmbed()
             .setColor(0x9590ee)
             .setTitle(doujin.titles.pretty.length > 0 ? doujin.titles.pretty : doujin.titles.english.length > 0 ? doujin.titles.english : doujin.titles.japanese)
@@ -57,7 +58,13 @@ export abstract class NHentaiCommands {
             .setImage(doujin.cover.url)
             .addField('Favorites', `${doujin.favorites}`, true)
             .addField('Length', `${doujin.length}`, true)
-            .addField('Tags', `${doujin.tags.all.map(tag => tag.name).join(', ')}`)
+            .addField('Language', doujin.tags.languages.map(language => client.utils.toProperCase(language.name)).join(', ') || 'Japanese', true)
+            .addField('Parody', doujin.tags.parodies.map(parody => parody.name).join(', ') || 'False', true)
+            .addField('Characters', doujin.tags.characters.map(character => character.name).join(', ') || 'None specified', true)
+            .addField(client.constants.zws, client.constants.zws, true)
+            .addField('Groups', doujin.tags.groups.map(group => group.name).join(', ') || 'None specified', true)
+            .addField('Artists', doujin.tags.artists.map(artist => artist.name).join(', ') || 'None specified', true)
+            .addField('Tags', doujin.tags.tags.map(tag => tag.name).join(', ') || 'None specified', false)
             .setFooter({ text: `${doujin.id}` })
 
         const pages = doujin.pages.map((p, i) => {
